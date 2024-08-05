4715b7275f401413703845a56e54e0c0cc488bb1
Samuel Uribe <suribe893@soyudemedellin.edu.co>
2024-08-04 19:31:09 -0500

    Commit 1

---
+++ punto1.py
@@ -0,0 +1,8 @@
+def main():
+    nombre=(input("¡Cual es tu nombre?  "))
+    print(f"Tu nombre es: {nombre}")
+
+    edad=int(input("¿Cual es tu edad?"))
+    print(f"Tu edad es: {edad}")
+
+main()
---
+++ punto2.py
@@ -0,0 +1,9 @@
+def radio():
+    pi= 3.141592653589793
+    radio=int(input("Cual es el radio del circulo  "))
+    
+    total=pi*(radio**2)
+    
+    print(f"El total del area del circulo con radio {radio} es: {total} cm cuadrados")
+    
+radio()
